a6318fac570ef7569f36dc32612726aa34d6013b
Josmar Dias <josmarnet@gmail.com>
2019-06-05 02:12:39 -0300

    move loaders to request context to isolate loaders caches by request

--- api/graphql-schema.js
+++ api/graphql-schema.js
@@ -1,11 +1,4 @@
 const { makeExecutableSchema } = require('graphql-tools')
-const DataLoader = require('dataloader')
-const {
-  getProducts, //
-  getStores,
-  getStoresByProductIds,
-  getProductsByStoreIds,
-} = require('./data')
 
 // helps editor highlight support
 const gql = input => input[0]
@@ -30,31 +23,23 @@ const typeDefs = gql`
   }
 `
 
-const productLoader = new DataLoader(ids => getProducts(ids))
-
-const storeLoader = new DataLoader(ids => getStores(ids))
-
-const storesByProductIdLoader = new DataLoader(ids => getStoresByProductIds(ids))
-
-const productsByStoreIdLoader = new DataLoader(ids => getProductsByStoreIds(ids))
-
 const resolvers = {
   Query: {
     product: (parent, args, context, info) => {
-      return productLoader.load(args.id)
+      return context.loaders.productById.load(args.id)
     },
     store: (parent, args, context, info) => {
-      return storeLoader.load(args.id)
+      return context.loaders.storeById.load(args.id)
     },
   },
   Product: {
     stores: (parent, args, context, info) => {
-      return storesByProductIdLoader.load(parent.id)
+      return context.loaders.storesByProductId.load(parent.id)
     },
   },
   Store: {
     products: (parent, args, context, info) => {
-      return productsByStoreIdLoader.load(parent.id)
+      return context.loaders.productsByStoreId.load(parent.id)
     },
   },
 }
--- api/index.js
+++ api/index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const graphql = require('express-graphql')
+const DataLoader = require('dataloader')
+const { getProducts, getStores, getStoresByProductIds, getProductsByStoreIds } = require('./data')
 const schema = require('./graphql-schema')
 
 const app = express()
 
-app.use(graphql({
-  schema,
-  graphiql: true,
-}))
+const buildLoaders = () => ({
+  productById: new DataLoader(ids => getProducts(ids)),
+  storeById: new DataLoader(ids => getStores(ids)),
+  storesByProductId: new DataLoader(ids => getStoresByProductIds(ids)),
+  productsByStoreId: new DataLoader(ids => getProductsByStoreIds(ids)),
+})
+
+app.use(
+  graphql(request => ({
+    schema,
+    graphiql: true,
+    context: { loaders: buildLoaders() },
+  })),
+)
 
 app.listen(4000)
